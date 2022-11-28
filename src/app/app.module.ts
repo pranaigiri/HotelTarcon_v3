8f5cb8905e17821bed7a7bd33fd6115dae3cd1ce
Pankaj Rawat <pankaj.rawat@sibingroup.com>
2022-11-28 18:31:12 +0530

    Api Service & App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ import { DineinnComponent } from './home/dineinn/dineinn.component';
 import { NearbyComponent } from './home/nearby/nearby.component';
 import { PopulardestinationComponent } from './home/populardestination/populardestination.component';
 
-
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 // SERVICES
 import { EventEmitterService } from './event-emitter.service';
 
@@ -34,6 +35,7 @@ import { EventEmitterService } from './event-emitter.service';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { ApiService } from './service/api.service';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -64,8 +66,9 @@ import { getAnalytics } from "firebase/analytics";
     BrowserModule,
     AppRoutingModule,
     SwiperModule,
+    HttpClientModule,
   ],
-  providers: [EventEmitterService],
+  providers: [EventEmitterService,ApiService,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
