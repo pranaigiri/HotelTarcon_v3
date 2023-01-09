35b751da207baba60e4c36f095a2a6c5d4cade83
naushad <naushad.hussain@sibingroup.com>
2023-01-09 19:11:50 +0530

    contact us post

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -9,23 +9,40 @@ import { CountryISO, SearchCountryField } from "ngx-intl-tel-input";
 })
 export class ContactusComponent implements OnInit {
 
-  constructor(private apiService: ApiService) { 
+  constructor(private apiService: ApiService) {
 
   }
 
   ngOnInit() {
   }
 
-  form:any = {};
+  form: any = {};
   CountryISO = CountryISO; //for initial selected country enum in international telephone list
   preferredCountries: CountryISO[] = [CountryISO.India, CountryISO.Bhutan, CountryISO.Nepal, CountryISO.Bangladesh];
 
 
   postForm() {
-    // this.apiService.postData("ContactUs/PostContactUs", this.form).subscribe((res: any) => {
-      console.log(this.form);
-    // })
-    this.form ={};
+    console.log(this.form);
+    var sendObject={
+      contactus_form_subject:"",
+      contactus_form_message:"",
+      contactus_form_name:"",
+      contactus_form_email:"",
+      contactus_form_phone:"",
+      contactus_form_countrycode:""
+    }
+    sendObject.contactus_form_subject=this.form.subject
+    sendObject.contactus_form_message=this.form.message
+    sendObject.contactus_form_name=this.form.name
+    sendObject.contactus_form_email=this.form.email
+    sendObject.contactus_form_phone=this.form.phone.e164Number
+    sendObject.contactus_form_countrycode=this.form.phone.dialCode
+    console.log(sendObject);
+    
+    this.apiService.postData("https://localhost:7210/api/Common/PostContactUsForm", sendObject).subscribe((res: any) => {
+      console.log(sendObject);
+    })
+    this.form = {};
 
   }
 
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -11,17 +11,17 @@ export class ApiService {
     private readonly httpOptions = {
         headers: new HttpHeaders({
             'Content-type': 'application/json',
-            'ApiKey' : 'IcFq8zTB3gVz97y9GxouqnT5Np70P82fcQKicisDhtg='
+            'ApiKey': 'IcFq8zTB3gVz97y9GxouqnT5Np70P82fcQKicisDhtg='
         })
     }
 
-    constructor(private http: HttpClient,private toaster:HotToastService) {
+    constructor(private http: HttpClient, private toaster: HotToastService) {
         // var check:any = window["baseUrl"];
     }
 
     //GetData Method
     getData(url: any) {
-        return this.http.get(`${this.baseUrl}/${url}`,{
+        return this.http.get(`${this.baseUrl}/${url}`, {
             headers: this.httpOptions.headers
         }).pipe(
             map((res: any) => {
@@ -35,7 +35,7 @@ export class ApiService {
 
     //GetDataById Method
     getDataById(url: any, id: any) {
-        return this.http.get(`${this.baseUrl}/${url}/${id}`,{
+        return this.http.get(`${this.baseUrl}/${url}/${id}`, {
             headers: this.httpOptions.headers
         }).pipe(timeout(300000),
             map((res: any) => {
@@ -49,7 +49,8 @@ export class ApiService {
 
     //Post Method
     postData(url: any, obj: any) {
-        return this.http.post(`${this.baseUrl}/${url}`, obj,{
+        // return this.http.post(`${this.baseUrl}/${url}`, obj,{
+        return this.http.post(url, obj, {
             headers: this.httpOptions.headers
         }).pipe(timeout(300000),
             map((res: any) => {
@@ -63,7 +64,7 @@ export class ApiService {
 
     //Put Method
     putData(url: any, obj: any) {
-        return this.http.put(`${this.baseUrl}/${url}`, obj,{
+        return this.http.put(`${this.baseUrl}/${url}`, obj, {
             headers: this.httpOptions.headers
         }).pipe(timeout(300000),
             map((res: any) => {
@@ -77,7 +78,7 @@ export class ApiService {
 
     //Delete Method
     deleteDataById(url: any, id: any) {
-        return this.http.delete(`${this.baseUrl}/${url}/${id}`,{
+        return this.http.delete(`${this.baseUrl}/${url}/${id}`, {
             headers: this.httpOptions.headers
         }).pipe(timeout(300000),
             map((res: any) => {
@@ -92,23 +93,23 @@ export class ApiService {
     //Handler
     handleError(error: any): any {
         this.toaster.error('Oops , something went wrong.',
-        {
-          position: 'bottom-center',
-          style: {
-            border: '1px solid #713200',
-            padding: '16px',
-            color: '#713200',
-          },
-          iconTheme: {
-            primary: '#713200',
-            secondary: '#FFFAEE',
-          },
-        }
-      )
+            {
+                position: 'bottom-center',
+                style: {
+                    border: '1px solid #713200',
+                    padding: '16px',
+                    color: '#713200',
+                },
+                iconTheme: {
+                    primary: '#713200',
+                    secondary: '#FFFAEE',
+                },
+            }
+        )
     }
 
     async getDataPromise(url: any) {
-        return this.http.get(`${this.baseUrl}/${url}`,{
+        return this.http.get(`${this.baseUrl}/${url}`, {
             headers: this.httpOptions.headers
         }).pipe(
             map((res: any) => {
